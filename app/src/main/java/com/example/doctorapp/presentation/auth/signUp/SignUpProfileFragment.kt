e0bf6b60e247c2729545b55ac17c65c573cf5ed5
DoHuyHoang <dohoang225@gmail.com>
2024-08-14 23:36:52 +0700

    [UI] add profile

--- app/src/main/java/com/example/doctorapp/presentation/auth/signUp/SignUpProfileFragment.kt
+++ app/src/main/java/com/example/doctorapp/presentation/auth/signUp/SignUpProfileFragment.kt
@@ -6,26 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.chatapp.domain.core.base.BaseFragment
 import com.example.doctorapp.R
+import com.example.doctorapp.databinding.FragmentSignUpProfileBinding
 
-class SignUpProfileFragment : Fragment() {
+class SignUpProfileFragment : BaseFragment<FragmentSignUpProfileBinding, SignUpProfileViewModel>(R.layout.fragment_sign_up_profile) {
 
     companion object {
         fun newInstance() = SignUpProfileFragment()
     }
 
     private val viewModel: SignUpProfileViewModel by viewModels()
+    override fun getVM() = viewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        // TODO: Use the ViewModel
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_sign_up_profile, container, false)
-    }
 }
--- app/src/main/java/com/example/doctorapp/presentation/auth/signUp/SignUpProfileViewModel.kt
+++ app/src/main/java/com/example/doctorapp/presentation/auth/signUp/SignUpProfileViewModel.kt
@@ -1,7 +1,8 @@
 package com.example.doctorapp.presentation.auth.signUp
 
 import androidx.lifecycle.ViewModel
+import com.example.chatapp.domain.core.base.BaseViewModel
 
-class SignUpProfileViewModel : ViewModel() {
+class SignUpProfileViewModel : BaseViewModel() {
     // TODO: Implement the ViewModel
 }
