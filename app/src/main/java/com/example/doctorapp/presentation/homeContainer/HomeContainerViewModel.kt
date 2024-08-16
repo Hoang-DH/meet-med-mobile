cda1f00f9c12f4039a1b73d74b4fce27ba96b849
DoHuyHoang <dohoang225@gmail.com>
2024-08-16 08:15:25 +0700

    [UI] add bottom nav

---
+++ app/src/main/java/com/example/doctorapp/presentation/homeContainer/HomeContainerViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.doctorapp.presentation.homeContainer
+
+import androidx.lifecycle.ViewModel
+
+class HomeContainerViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
